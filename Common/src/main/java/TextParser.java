0f0d5d4cbfa4f8b9ba1b7bec662c9851e19f7c5f
RaghadQadah <raghadqadah99@gmail.com>
2021-07-08 16:24:51 +0300

    second commit

--- Common/src/main/java/TextParser.java
+++ Common/src/main/java/TextParser.java
@@ -8,12 +8,12 @@ import java.util.regex.Pattern;
 
 public class TextParser {
 
-    Person person=new Person();
+    Person person = new Person();
 
     public Person getPerson(String line) {
         person.setName(getName(line));
         person.setId(Long.parseLong(getId(line)));
-        List <String >job =new ArrayList<String>();
+        List<String> job = new ArrayList<>();
         job.add(getJob(line));
         person.setJobs(job);
         person.setAge(Integer.parseInt(getAge(line)));
@@ -24,98 +24,83 @@ public class TextParser {
         this.person = person;
     }
 
-    public  String getId(String line) {
-        String pattern = "(?i)id\\s+is\\s+(\\d+)";
-        Pattern r = Pattern.compile(pattern);
-        Matcher m = r.matcher(line);
+    public String getId(String line) {
 
-        String pattern1 = "(?i)with\\s+id\\s+(\\d+)";
-        Pattern r1 = Pattern.compile(pattern1);
-        Matcher m1 = r1.matcher(line);
+        List<String> patterns = new ArrayList<>(); // Create an ArrayList object
+        patterns.add("(?i)id\\s+is\\s+(\\d+)");
+        patterns.add("(?i)with\\s+id\\s+(\\d+)");
 
+        for (String pattern : patterns) {
+            Pattern r = Pattern.compile(pattern);
+            Matcher m = r.matcher(line);
 
-        if (m.find()) {
-            return m.group(1);
+            if (m.find()) {
+                return m.group(1);
+            }
+
         }
-        if (m1.find()) {
-            return m1.group(1);
-        }
-
-
         return null;
+
+
     }
 
-    public  String getJob(String line) {
-        String pattern = "(?i)am\\s+a\\s+([a-zA-Z]+)";
-        Pattern r = Pattern.compile(pattern);
-        Matcher m = r.matcher(line);
+    public String getJob(String line) {
 
-        String pattern1 = "(?i)\\s+as\\s+a\\s+([a-zA-Z]+)";
-        Pattern r1 = Pattern.compile(pattern1);
-        Matcher m1 = r1.matcher(line);
 
-        String pattern2 = "(?i)job\\s+is\\s+([a-zA-Z]+)";
-        Pattern r2 = Pattern.compile(pattern2);
-        Matcher m2 = r2.matcher(line);
+        List<String> patterns = new ArrayList<>(); // Create an ArrayList object
+        patterns.add("(?i)am\\s+a\\s+([a-zA-Z]+)");
+        patterns.add("(?i)\\s+as\\s+a\\s+([a-zA-Z]+)");
+        patterns.add("(?i)job\\s+is\\s+([a-zA-Z]+)");
 
-        if (m.find()) {
-            return m.group(1);
+        for (String pattern : patterns) {
+            Pattern r = Pattern.compile(pattern);
+            Matcher m = r.matcher(line);
+
+            if (m.find()) {
+                return m.group(1);
+            }
+
         }
-        if (m1.find()) {
-            return m1.group();
-        }
-        if (m2.find()) {
-            return m2.group(1);
-        }
-
         return null;
+
+
     }
 
     public String getAge(String line) {
-        String pattern = "(?i)age\\s+(\\d+)";
-        Pattern r = Pattern.compile(pattern);
-        Matcher m = r.matcher(line);
 
-        String pattern1 = "(?i)(\\d+)\\s+years\\s+old";
-        Pattern r1 = Pattern.compile(pattern1);
-        Matcher m1 = r1.matcher(line);
+        List<String> patterns = new ArrayList<>(); // Create an ArrayList object
+        patterns.add("(?i)\\s+age\\s+(\\d+)");
+        patterns.add("(?i)(\\d+)\\s+years\\s+old");
 
+        for (String pattern : patterns) {
+            Pattern r = Pattern.compile(pattern);
+            Matcher m = r.matcher(line);
 
-        if (m.find()) {
-            return m.group(1);
+            if (m.find()) {
+                return m.group(1);
+            }
+
         }
-        if (m1.find()) {
-            return m1.group(1);
-        }
-
         return null;
+
+
     }
 
     public String getName(String line) {
-        String pattern = "(?i)(?:i\\s+am\\s+([a-zA-Z]{3,}))";
-        Pattern r = Pattern.compile(pattern);
-        Matcher m = r.matcher(line);
+        List<String> patterns = new ArrayList<>(); // Create an ArrayList object
+        patterns.add("(?i)(?:i\\s+am\\s+([a-zA-Z]{3,}))");
+        patterns.add("(?i)name\\s+([a-zA-Z]{3,})");
+        patterns.add("(?i)name\\s+is\\s+([a-zA-Z]+)");
 
-        String pattern1 = "(?i)name\\s+([a-zA-Z]{3,})";
-        Pattern r1 = Pattern.compile(pattern1);
-        Matcher m1 = r1.matcher(line);
+        for (String pattern : patterns) {
+            Pattern r = Pattern.compile(pattern);
+            Matcher m = r.matcher(line);
 
-        String pattern2 = "(?i)name\\s+is\\s+([a-zA-Z]+)";
-        Pattern r2 = Pattern.compile(pattern2);
-        Matcher m2 = r2.matcher(line);
+            if (m.find()) {
+                return m.group(1);
+            }
 
-
-        if (m.find()) {
-            return m.group(1);
         }
-        if (m1.find()) {
-            return m1.group(1);
-        }
-        if (m2.find()) {
-            return m2.group(1);
-        }
-
-
         return null;
     }
 }
